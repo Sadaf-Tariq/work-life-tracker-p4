2964c478b05fbb73451088359c1927a2a4fd9635
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2023-10-27 22:52:19 +0000

    Add template and views for home page

--- recipe_blog/views.py
+++ recipe_blog/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Recipe, Rating
 
+
 class RecipeList(generic.ListView):
 
     model = Recipe
     queryset = Recipe.objects.all().order_by('-created_on')
     template_name = "index.html"
     paginated_by = 6
+    
+
+class RatingList():
+    model = Rating
+    queryset = Rating.objects.select_related('recipe')
